6bfb6e8d2b5dd17eaeef345d96f4cbd3282d57f5
Priyanka <40425175+pbhakuni@users.noreply.github.com>
2020-01-31 12:12:01 +0530

    Update Palindrome_Program4.py

--- Palindrome_Program4.py
+++ Palindrome_Program4.py
@@ -1,12 +1,15 @@
-multiply = 1
+# Program 4 - largest palindrome of 3 digit product
+# https://projecteuler.net/problem=4
 
 def compute():
+    li = []
     for i in range(1000,100,-1):
         for j in range(1000,100,-1):
             multiply = i *j
             if str(multiply) == str(multiply)[::-1]:
-                return(multiply)
-
+                li.append(multiply)
+    return(max(li))            
 
 print(compute())
-        
+
+#913 993 = 906609
