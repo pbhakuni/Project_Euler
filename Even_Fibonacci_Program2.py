1aa19876992ec16a24a5957251a033eaf1ecd1e1
Priyanka <40425175+pbhakuni@users.noreply.github.com>
2020-02-11 14:59:11 +0530

    Optimized the program

--- Even_Fibonacci_Program2.py
+++ Even_Fibonacci_Program2.py
@@ -2,16 +2,20 @@
 # Project Euler Solutions
 # https://projecteuler.net/problem=2
 
-def Fibonacci():
+
+t = int(input().strip())
+
+for a0 in range(t):
     first = 1
     second = 2
-    add = 0
-    while first <=4000000:
-        if first%2 == 0:
-            add = add+first
-        first,second = second,first+second
-    return(str(add))
-    
-    
-if __name__ == "__main__":
-    print(Fibonacci())
+    third = 3   
+    sum = 2
+    n = int(input().strip())
+    while (third<n):  # 5 < 10
+        first = second #5
+        second = third #3
+        if third%2==0: 
+            sum = sum +third #10
+        third = first + second #8     
+    print(sum)        
+
